4ae41fb5d4b774acc79d076f213ed986bccd5a07
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2022-09-28 17:01:42 +0300

    fix

--- Assets/Scrpts/Behaiovurs/PlayerMovementBehaviour.cs
+++ Assets/Scrpts/Behaiovurs/PlayerMovementBehaviour.cs
@@ -37,7 +37,7 @@ public class PlayerMovementBehaviour : MonoBehaviour
 
     private void BallCreateProcess()
     {
-        var ball = Instantiate(_ball, new Vector3(Random.Range(-10f, 10f), 0, Random.Range(300f, 50f)), Quaternion.identity);
+        var ball = Instantiate(_ball, new Vector3(Random.Range(-10f, 10f), 0, Random.Range(30f, 50f)), Quaternion.identity);
         var ball1 = Instantiate(_ball, new Vector3(Random.Range(-10f, 10f), 0, Random.Range(30f, 50f)), Quaternion.identity);
         var ball2 = Instantiate(_ball, new Vector3(Random.Range(-10f, 10f), 0, Random.Range(30f, 50f)), Quaternion.identity);
         var ball3 = Instantiate(_ball, new Vector3(Random.Range(-10f, 10f), 0, Random.Range(30f, 50f)), Quaternion.identity);
