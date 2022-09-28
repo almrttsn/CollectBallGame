b32777d450e3541d87fa1f4885ba2e3b48823e51
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2022-09-28 16:44:17 +0300

    ball create process

--- Assets/Scrpts/Behaiovurs/PlayerMovementBehaviour.cs
+++ Assets/Scrpts/Behaiovurs/PlayerMovementBehaviour.cs
@@ -8,8 +8,14 @@ public class PlayerMovementBehaviour : MonoBehaviour
     [SerializeField] private GameObject _player;
     [SerializeField] private float _playerMovementSpeed;
     [SerializeField] private float _playerMovementFactor;
+    [SerializeField] private GameObject _ball;
 
     private bool _isPlayerLocked;
+
+    private void Start()
+    {
+        BallCreateProcess();
+    }
 
     private void Update()
     {
@@ -27,6 +33,14 @@ public class PlayerMovementBehaviour : MonoBehaviour
                 transform.position += new Vector3(_playerMovementSpeed, 0, 0) * _playerMovementFactor * Time.deltaTime;
             }
         }
+    }
+
+    private void BallCreateProcess()
+    {
+        var ball = Instantiate(_ball, new Vector3(Random.Range(-10f, 10f), 0, Random.Range(10f, 30f)), Quaternion.identity);
+        var ball1 = Instantiate(_ball, new Vector3(Random.Range(-10f, 10f), 0, Random.Range(10f, 30f)), Quaternion.identity);
+        var ball2 = Instantiate(_ball, new Vector3(Random.Range(-10f, 10f), 0, Random.Range(10f, 30f)), Quaternion.identity);
+        var ball3 = Instantiate(_ball, new Vector3(Random.Range(-10f, 10f), 0, Random.Range(10f, 30f)), Quaternion.identity);
     }
 
     private void OnTriggerEnter(Collider other)
