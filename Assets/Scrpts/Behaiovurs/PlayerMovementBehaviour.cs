f247d05db8c645142e81cafdffae0e894d8778c6
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2022-09-28 15:49:10 +0300

    player movement process implemented

---
+++ Assets/Scrpts/Behaiovurs/PlayerMovementBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject _triggerObject;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float _playerMovementSpeed;
+    [SerializeField] private float _playerMovementFactor;
+
+    private bool _isPlayerLocked;
+
+    private void Update()
+    {
+        if (_isPlayerLocked == false)
+        {
+            transform.position += new Vector3(0, 0, _playerMovementSpeed) * _playerMovementFactor * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += new Vector3(-_playerMovementSpeed, 0, 0) * _playerMovementFactor * Time.deltaTime;
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += new Vector3(_playerMovementSpeed, 0, 0) * _playerMovementFactor * Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == _triggerObject)
+        {
+            Debug.Log("Triggered");
+            _isPlayerLocked = true;
+        }
+    }
+}
