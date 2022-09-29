6b00baf7fbb41d0d2b1c1e1b209c256bae2c271d
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2022-09-29 11:28:34 +0300

    game manager system implanted

--- Assets/Scrpts/Managers/GameManager.cs
+++ Assets/Scrpts/Managers/GameManager.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class GameManager : CustomBehaviour
 {
     public UIManager UIManager => _uiManager;
-    
-    [SerializeField]private UIManager _uiManager;
+    public PlayerMovementBehaviour PlayerMovementBehaviour => _playerMovementBehaviour;
+    public BallBehaviour BallBehaviour => _ballBehaviour;
+     
+    [SerializeField] private UIManager _uiManager;
+    [SerializeField] private PlayerMovementBehaviour _playerMovementBehaviour;
+    [SerializeField] private BallBehaviour _ballBehaviour;
 
     private void Awake()
     {
         Application.targetFrameRate = 60;
+    }
 
+    private void Initialize()
+    {
         _uiManager.Initialize(this);
+        _playerMovementBehaviour.Initialize(this);
+        _ballBehaviour.Initialize(this);
     }
 }
 
