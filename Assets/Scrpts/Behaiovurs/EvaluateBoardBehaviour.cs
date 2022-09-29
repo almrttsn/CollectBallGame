1a71278c594c0adebffad39285ecddda16ff0454
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2022-09-29 12:40:00 +0300

    collision process

--- Assets/Scrpts/Behaiovurs/EvaluateBoardBehaviour.cs
+++ Assets/Scrpts/Behaiovurs/EvaluateBoardBehaviour.cs
@@ -6,9 +6,18 @@ using UnityEngine;
 public class EvaluateBoardBehaviour : MonoBehaviour
 {
     private GameManager _gameManager;
+    private BallBehaviour _droppedBall;
 
     public void Initialize(GameManager gameManager)
     {
         _gameManager = gameManager;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            Debug.Log("Ball dropped");
+        }
     }
 }
