664a9964c50553f11d861b88042f8277b99e9405
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2022-09-29 12:03:03 +0300

    game manager system applied

--- Assets/Scrpts/Managers/GameManager.cs
+++ Assets/Scrpts/Managers/GameManager.cs
@@ -7,14 +7,18 @@ public class GameManager : CustomBehaviour
     public UIManager UIManager => _uiManager;
     public PlayerMovementBehaviour PlayerMovementBehaviour => _playerMovementBehaviour;
     public BallBehaviour BallBehaviour => _ballBehaviour;
-     
+    public EvaluateBoardBehaviour EvaluateBoardBehaviour => _evaluateBoardBehaviour;
+
+
     [SerializeField] private UIManager _uiManager;
     [SerializeField] private PlayerMovementBehaviour _playerMovementBehaviour;
     [SerializeField] private BallBehaviour _ballBehaviour;
+    [SerializeField] private EvaluateBoardBehaviour _evaluateBoardBehaviour;
 
     private void Awake()
     {
         Application.targetFrameRate = 60;
+        Initialize();
     }
 
     private void Initialize()
@@ -22,6 +26,7 @@ public class GameManager : CustomBehaviour
         _uiManager.Initialize(this);
         _playerMovementBehaviour.Initialize(this);
         _ballBehaviour.Initialize(this);
+        _evaluateBoardBehaviour.Initialize(this);
     }
 }
 
