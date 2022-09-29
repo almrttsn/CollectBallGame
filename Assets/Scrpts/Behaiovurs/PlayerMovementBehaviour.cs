4ec59c330f798e50451d48b9e74ea5137d2d2416
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2022-09-29 11:28:20 +0300

    player movement behaviour fix

--- Assets/Scrpts/Behaiovurs/PlayerMovementBehaviour.cs
+++ Assets/Scrpts/Behaiovurs/PlayerMovementBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,14 +9,13 @@ public class PlayerMovementBehaviour : MonoBehaviour
     [SerializeField] private GameObject _player;
     [SerializeField] private float _playerMovementSpeed;
     [SerializeField] private float _playerMovementFactor;
-    [SerializeField] private GameObject _ball;
 
     private bool _isPlayerLocked;
 
-    private void Start()
+    public void Initialize(GameManager gameManager)
     {
-        BallCreateProcess();
-    }
+        _isPlayerLocked = false;
+    }    
 
     private void Update()
     {
@@ -33,14 +33,6 @@ public class PlayerMovementBehaviour : MonoBehaviour
                 transform.position += new Vector3(_playerMovementSpeed, 0, 0) * _playerMovementFactor * Time.deltaTime;
             }
         }
-    }
-
-    private void BallCreateProcess()
-    {
-        var ball = Instantiate(_ball, new Vector3(Random.Range(-10f, 10f), 0, Random.Range(30f, 50f)), Quaternion.identity);
-        var ball1 = Instantiate(_ball, new Vector3(Random.Range(-10f, 10f), 0, Random.Range(30f, 50f)), Quaternion.identity);
-        var ball2 = Instantiate(_ball, new Vector3(Random.Range(-10f, 10f), 0, Random.Range(30f, 50f)), Quaternion.identity);
-        var ball3 = Instantiate(_ball, new Vector3(Random.Range(-10f, 10f), 0, Random.Range(30f, 50f)), Quaternion.identity);
     }
 
     private void OnTriggerEnter(Collider other)
