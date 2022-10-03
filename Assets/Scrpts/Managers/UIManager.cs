578d21b4417689529b58c6ef96997e56d7e21305
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2022-10-03 17:38:22 +0300

    UI manager edit

--- Assets/Scrpts/Managers/UIManager.cs
+++ Assets/Scrpts/Managers/UIManager.cs
@@ -9,13 +9,14 @@ public class UIManager : CustomBehaviour
 {
     public MainMenuPanel MainMenuPanel => _mainMenuPanel;
     [SerializeField] private MainMenuPanel _mainMenuPanel;
+    [SerializeField] private FinishPanel _finishPanel;
 
     private List<UIPanel> UIPanels;
 
     public override void Initialize(GameManager gameManager)
     {
         base.Initialize(gameManager);
-        UIPanels = new List<UIPanel> {_mainMenuPanel};
+        UIPanels = new List<UIPanel> {_mainMenuPanel,_finishPanel};
 
         UIPanels.ForEach(x =>
         {
@@ -23,8 +24,10 @@ public class UIManager : CustomBehaviour
             x.gameObject.SetActive(false);
         });
         //hudPanel.ShowPanel();
-
+        _finishPanel.ShowPanel();
     }
+
+    
 
     //public void ClosePanelsOnGoHome()
     //{
