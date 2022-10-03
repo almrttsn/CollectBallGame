2ba83bdddea58e885430e3dac45ed0d0d1c12958
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2022-10-03 17:34:36 +0300

    ball behaviour connected to event

--- Assets/Scrpts/Behaiovurs/BallBehaviour.cs
+++ Assets/Scrpts/Behaiovurs/BallBehaviour.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class BallBehaviour : MonoBehaviour
 {
-    //[SerializeField] List<GameObject> _ballList = new List<GameObject>();
+    [SerializeField] List<GameObject> _ballList = new List<GameObject>();
     [SerializeField] private GameObject _ball;
     [SerializeField] int _desiredBallCount;
-    private GameManager _gameManager;
 
-    public void Initialize(GameManager gameManager)
-    {
-        _gameManager = gameManager;
+    public void Initialize()
+    {        
         BallCreateProcess();
+        //_gameManager.EventManager.OnLevelStarted += BallCreateProcess;
     }
 
     private void BallCreateProcess()
     {
+        //if(_ballList.Count > 0)
+        //{
+        //    for (int i = 0; i < _ballList.Count; i++)
+        //    {
+        //        Destroy(_ballList[i].gameObject);
+        //    }
+        //}
+
+        //_ballList = new List<GameObject>();
+
         for (int i = 0; i < _desiredBallCount; i++)
         {
-            Instantiate(_ball, new Vector3(Random.Range(-5f, 5f), 0, Random.Range(30f, 70f)), Quaternion.identity);
+            var ball = Instantiate(_ball, new Vector3(Random.Range(-5f, 5f), 0, Random.Range(30f, 70f)), Quaternion.identity,transform);
+            //_ballList.Add(ball);
         }
     }
 
